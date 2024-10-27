933c63baf06865cbb71baff9c0276e3a3432bf20
hemzadev <hamzabenazza.dev@gmail.com>
2024-10-27 20:33:01 +0100

    Eureka to Consul part

---
+++ modMs/src/main/java/com/hmzadev/modms/ModMsApplication.java
@@ -0,0 +1,13 @@
+package com.hmzadev.modms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModMsApplication.class, args);
+    }
+
+}
